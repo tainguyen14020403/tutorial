d2c88d60f02e686011bba6b9148b937b0938cf4a
tainguyen14020403 <tainguyen14020403@gmail.com>
2016-07-05 16:26:18 +0700

    Basic login

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,17 +7,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
